d387632a785045fe961f517f44e57dd881e734dc
Sashank576 <csashank123@gmail.com>
2024-09-27 00:14:58 +0200

    Tweaked Parameters

--- Synthetic Group Creation/config.py
+++ Synthetic Group Creation/config.py
@@ -3,7 +3,7 @@
 group_sizes_to_create = [2,4,8] # [2, 3, 4, 5, 6, 7, 8]
 group_similarity_to_create = ["RANDOM", "SIMILAR", "DIVERGENT", "SIMILAR_ONE_DIVERGENT"]
 group_number = 20
-similar_threshold = 0.5
-dissimilar_threshold = 0.1
+similar_threshold = 0.6
+dissimilar_threshold = 0.15
 shared_ratings = 5
 
