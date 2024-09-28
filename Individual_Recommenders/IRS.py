2be5f71552e65268a1b931d8bcf62cbb0bdb72b0
Sebastian Schmülling <sebastian.schmuelling@gmail.com>
2024-09-28 18:21:41 +0200

    Implementation of group recommender

---
+++ Individual_Recommenders/IRS.py
@@ -0,0 +1,17 @@
+from lenskit.algorithms import Recommender  
+from lenskit.algorithms.user_knn import UserUser 
+
+
+
+
+
+
+
+class Individual_Recommenders: 
+
+    
+    def useruser_individual_recommender_train(self, train_df):  
+        item_item = ItemItem(15, min_nbrs=3)  # Minimum (3) and maximum (15) number of neighbors to consider  
+        recsys = Recommender.adapt(item_item)  
+        recsys.fit(train_df)  
+        return recsys  
