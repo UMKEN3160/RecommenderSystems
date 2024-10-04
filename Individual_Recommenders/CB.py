7bf6abfa65d58140f288e972223ea002029af6c1
Sashank576 <csashank123@gmail.com>
2024-10-04 15:13:21 +0200

    Merge conflict

--- Individual_Recommenders/CB.py
+++ Individual_Recommenders/CB.py
@@ -11,46 +11,6 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 class ContentBasedRecommender:
     def __init__(self, ratings, wines):
         self.ratings = ratings
-<<<<<<< HEAD
-        self.wines = wines.reset_index(drop=True)
-        self.similarity_matrix = None
-        self.wine_id_to_index = None
-
-    def fit(self):
-        # create feature matrix (e.g., one-hot encode categorical features)
-        features = pd.get_dummies(self.wines[['Type', 'Country', 'Body']])
-        # compute the cosine similarity matrix
-        self.similarity_matrix = cosine_similarity(features)
-        # Create a mapping from WineID to index
-        self.wine_id_to_index = pd.Series(self.wines.index, self.wines['WineID']).to_dict()
-
-
-    def recommend(self, user_id, top_n=5):
-        # get wines rated by the user
-        user_wines = self.ratings[self.ratings['UserID'] == user_id]
-        if user_wines.empty:
-            print("user has rated no wines")
-            return []
-        
-        # get the indices of the wines the user has rated
-        rated_indices = user_wines['WineID'].values
-        
-        # compute similarity scores for rated wines
-        scores = pd.Series(0, index=self.wines['WineID'])
-        for wine_id in rated_indices:
-            if wine_id in self.wines['WineID'].values:
-                index = self.wine_id_to_index[wine_id]
-                similar_scores = self.similarity_matrix[index]
-                scores += similar_scores
-            else:
-                print("WineID not in wine dataset")
-        # exclude wines already rated by the user
-        scores = scores[~scores.index.isin(rated_indices)]
-        
-        # get the top 5 recommendations
-        recommendations = scores.nlargest(top_n).index.tolist()
-        return recommendations
-=======
         self.wines = wines
         self.encoder = OneHotEncoder()
         self.scaler = StandardScaler()  
@@ -84,7 +44,6 @@ class ContentBasedRecommender:
         # Wines user has already rated
         user_ratings = self.ratings[self.ratings['UserID'] == user_id]
         rated_wines = user_ratings['WineID'].values
->>>>>>> 24a28ffb67be08e6b70947685c55b311a7bcffe8
 
         # Unrated wines
         unrated_wines = self.wines[~self.wines['WineID'].isin(rated_wines)]
