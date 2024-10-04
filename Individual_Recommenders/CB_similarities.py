bcbf306f7807795674221808d67aa493bbe8056e
Sashank576 <csashank123@gmail.com>
2024-10-04 15:26:22 +0200

    Changed CB_sim

--- Individual_Recommenders/CB_similarities.py
+++ Individual_Recommenders/CB_similarities.py
@@ -5,37 +5,39 @@ from sklearn.metrics.pairwise import cosine_similarity
 class CBRecommender:
     def __init__(self, ratings, wines):
         self.ratings = ratings
-        self.wines = wines
+        self.wines = wines.reset_index(drop=True)
         self.similarity_matrix = None
-
-    def fit(self, train_ratings):
-        # Create feature matrix (e.g., one-hot encode categorical features)
+        self.wine_id_to_index = None
+    def fit(self):
+        # create feature matrix (e.g., one-hot encode categorical features)
         features = pd.get_dummies(self.wines[['Type', 'Country', 'Body']])
-        # Compute the cosine similarity matrix based on wine features
+        # compute the cosine similarity matrix
         self.similarity_matrix = cosine_similarity(features)
-        # Set the training ratings as an attribute to filter users' ratings
-        self.train_ratings = train_ratings
-
+        # Create a mapping from WineID to index
+        self.wine_id_to_index = pd.Series(self.wines.index, self.wines['WineID']).to_dict()
     def recommend(self, user_id, top_n=5):
-        # Get wines rated by the user in the training set
-        user_wines = self.train_ratings[self.train_ratings['UserID'] == user_id]
+        # get wines rated by the user
+        user_wines = self.ratings[self.ratings['UserID'] == user_id]
         if user_wines.empty:
+            print("user has rated no wines")
             return []
         
-        # Get the indices of the wines the user has rated
+        # get the indices of the wines the user has rated
         rated_indices = user_wines['WineID'].values
         
-        # Compute similarity scores for rated wines
+        # compute similarity scores for rated wines
         scores = pd.Series(0, index=self.wines['WineID'])
         for wine_id in rated_indices:
-            index = self.wines.index[self.wines['WineID'] == wine_id].tolist()[0]
-            similar_scores = self.similarity_matrix[index]
-            scores += similar_scores
-            
-        # Exclude wines already rated by the user
+            if wine_id in self.wines['WineID'].values:
+                index = self.wine_id_to_index[wine_id]
+                similar_scores = self.similarity_matrix[index]
+                scores += similar_scores
+            else:
+                print("WineID not in wine dataset")
+        # exclude wines already rated by the user
         scores = scores[~scores.index.isin(rated_indices)]
         
-        # Get the top_n recommendations
+        # get the top 5 recommendations
         recommendations = scores.nlargest(top_n).index.tolist()
         return recommendations
 
